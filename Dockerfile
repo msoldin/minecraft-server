5cc886f5d70f786c38e67c621af7639218624981
soldin_m <msoldin@Marvin-Desktop.localdomain>
2025-11-11 14:34:05 +0100

    Fix startup

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,10 @@ FROM eclipse-temurin:25-jre-alpine
 WORKDIR /opt/minecraft
 RUN --mount=target=/build,source=build /build/install-packages.sh
 COPY --chmod=755 /scripts/start.sh ./scripts/start.sh
-COPY --chmod=755 /scripts/install-server.py ./scripts/install-server.py
+COPY --chmod=755 /scripts/launcher.py ./scripts/launcher.py
 
 WORKDIR /data
 
-ENV MEMORY_SIZE=1G
 ENV UID=1000
 ENV GID=1000
 
--- scripts/start.sh
+++ scripts/start.sh
@@ -14,17 +14,9 @@ if ! id "$DOCKER_USER" >/dev/null 2>&1; then
 
     addgroup --gid $GROUP_ID $DOCKER_GROUP
     adduser $DOCKER_USER --shell /bin/sh --uid $USER_ID --ingroup $DOCKER_GROUP --disabled-password --gecos ""
-    mkdir -p /config
     mkdir -p /opt/minecraft
     chown -vR $USER_ID:$GROUP_ID /data
-    chown -vR $USER_ID:$GROUP_ID /config
     chown -vR $USER_ID:$GROUP_ID /opt/minecraft
 fi
 
-su-exec "${DOCKER_USER}:${DOCKER_GROUP}" python /opt/minecraft/scripts/install-server.py
-source /config/minecraft_version.sh
-exec su-exec "${DOCKER_USER}:${DOCKER_GROUP}" java \
-    -Xms"${MEMORY_SIZE}" -Xmx"${MEMORY_SIZE}" \
-    -Dlog4j2.formatMsgNoLookups=true \
-    -XX:+UseZGC -XX:+AlwaysPreTouch -XX:+UseStringDeduplication -XX:+UseCompactObjectHeaders \
-    -jar /opt/minecraft/"${MINECRAFT_VERSION}-server.jar" nogui
+su-exec "${DOCKER_USER}:${DOCKER_GROUP}" python /opt/minecraft/scripts/launcher.py
